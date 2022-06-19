d690af5bbed86684688c865a5834ccfb5cfb2887
Galozy, Adrian <70019389+Galozy1988@users.noreply.github.com>
2022-06-19 21:26:11 +0200

    zentrieren layout

--- index.php
+++ index.php
@@ -6,22 +6,42 @@
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css" rel="stylesheet">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
 
+    <style>
+    .backround{
+        height: 100vh;
+        background-image:url(https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 1300px;
+        filter: alpha(opacity=25);
+       background: -moz-opacity: 0.25;
+
+    }
+
+
+    </style>
+
 </head>
 
 
-<body class="allgemein">
+<body class="background">
+
+
 
 <p>
 <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" style = "height: 80px;width: 200px;margin 0px auto">
+
 </p>
-    <p style="position:absolute;top:100px;left:350px;">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" width="800" height="600"
-         style="filter:alpha(opacity=25); -moz-opacity: 0.25; opacity: 0.25;">
-    </p>
+
+   <div>
+    <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" width="800" height="600"-->
+
+    </div>
+
 
     <h3 style="text-align: center ">Fahrplan</h3>
 
-    <p>
+    <p style="text-align: center">
         <label for="Route">Wähle einen Routentyp:</label>
         <select id="Route" name="Route">
             <option value="direct">direkt</option>
@@ -29,12 +49,12 @@
         </select>
     </p>
 
-    <p>
+    <p style="text-align: center">
         <label for="Route">Wähle eine Abfahrtszeit:</label>
-        <input type="time" id="depart_Time" name="depart_Time">
+        <input type="time" id="departTime" name="departTime">
     </p>
 
-    <p>
+    <p style="text-align: center">
         <input type="text" placeholder="Start" id="startStop" name="startStop">
         <input type="text" placeholder="Ziel" id="endStop" name="endStop">
         <input type="button" onclick="onSearch()" value="Suche">
@@ -49,10 +69,10 @@
     <h3 style="text-align: center " >Haltestellen</h3>
     <div id="detailRoute"></div>
 
-
+<hr>
 <h3  style="text-align: center " >Karte</h3>
 <!--The div element for the map -->
-<div id="map" style="width: 900px; height: 450px;border-radius: 20px"></div>
+<div id="map" style="width: 900px; height: 450px;border-radius: 20px; margin: auto"></div>
 <script src="js/main.js"></script>
 </body>
 </html>
