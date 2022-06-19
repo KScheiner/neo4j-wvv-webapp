68c0b4d178526c3b4d545c33afd06c7a3d75b96b
Galozy, Adrian <70019389+Galozy1988@users.noreply.github.com>
2022-06-19 11:28:33 +0200

    Update for main

--- index.php
+++ index.php
@@ -7,35 +7,36 @@
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
 </head>
 <body>
-<img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" style = "height: 80px;width: 200px">
-
-<h3> description</h3>
-
-<p>Eingabefeld(Start)
-    <br>Eingabefeld(Ziel)
-    <br>Eingabefeld(Uhrzeit)</p>
-
-<p><a>
-    <button>Suchen</button>
-    </a>
 
 
 
-</p>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" style = "height: 80px;width: 200px">
 
-<hr>
-<h3>Ausgabe</h3>
+    <h3> description</h3>
 
-<?php
+    <p>
+        <input type="text" placeholder="Start" id="startStop" name="startStop">
+        <input type="text" placeholder="Ziel" id="endStop" name="endStop">
+        <input type="button" onclick="onSearch()" value="Suche">
+    </p>
+
+
+
+
+
+    </p>
+
+    <hr>
+    <h3>Ausgabe</h3>
+
+    <?php
 include ("neo4j.php")?>
 
 
-
-
-<hr>
-<h3>Map</h3>
+    <hr>
+<h3>Mapbox Demo</h3>
 <!--The div element for the map -->
 <div id="map" style="width: 900px; height: 450px;border-radius: 20px"></div>
-<script src="map.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
