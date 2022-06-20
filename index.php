e30f3bfa6f243ea6db3fd414937f32e7de7bcd29
kousman <ko.scheiner@gmail.com>
2022-06-20 18:34:27 +0200

    Added fonts, cleaned up index.php

--- index.php
+++ index.php
@@ -5,44 +5,23 @@
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css" rel="stylesheet">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
-<!--
     <style>
-    .backround{
-        height: 100vh;
-        background-image:url(https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png);
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: 1300px;
-        filter: alpha(opacity=25);
-       background: -moz-opacity: 0.25;
-
-    }
-
-
+        @font-face {
+            font-family: "roboto";
+            src: url("fonts/Roboto-Regular.ttf");
+        }
+        p, span, h1, h2 {
+            font-family: "roboto", sans-serif;
+        }
+        hr {
+            margin: 35px;
+        }
     </style>
--->
 </head>
-
-
-<body style="background-color: white; padding-left: 200px;padding-right: 200px">
-
-
-<!--
-<p>
-<img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" style = "height: 100px;width: 240px;margin 0px auto">
-
-</p>
--->
-
-
-   <div>
-    <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Neo4j-logo_color.png" width="800" height="600"-->
-
-    </div>
-
+<body style="background-color: white; padding-left: 200px;padding-right: 200px;margin: 0">
 <div style="background-color: aliceblue">
 
-<div style=" background-image: url(https://www.wvv.de/media-wvv/mobilitaet/bilder/bus-und-strassenbahn/wvv_elektrobus.jpg);background-repeat: no-repeat; background-size: 1000px; height: 400px">
+<div style=" background-image: url(https://www.wvv.de/media-wvv/mobilitaet/bilder/bus-und-strassenbahn/wvv_elektrobus.jpg);background-repeat: no-repeat; background-size: 1000px; height: 400px; background-size: cover">
     <img src="https://legalhelper.eu/ccm19/public/logo/5c5986e/bdf04a6/968e274" style="text-align:left; padding-left: 10px; height: 100px; width: 200px">
 
 </div>
@@ -74,35 +53,14 @@
         <input type="text" placeholder="Ziel" id="endStop" name="endStop">
         <input type="button" onclick="onSearch()" value="Suche">
     </p>
-<div style="height:25px"></div>
-
-
-
-
-
-
-
-    <hr>
-<!--
-    <form action="php/direct.php" method="get">
-
-
-        <div >
-            <label for="text"> <h3 style="text-align: center " >Haltestellen</h3></label>
-            <textarea id="detailRoute" name="detailRoute" cols="50" rows="20"></textarea>
-
-        </div>
-    </form>
--->
-<h2 style="text-align: center;" ><u>Haltestellen</u></h2>
-<div id="detailRoute" style="height: auto; margin-left: auto; margin-right: auto; width: 25em; border-color: darkgrey; border-width: 2px; border-style: solid; background-color: ivory"></div>
-<div style="height:25px"></div>
 <hr>
-<div style="height:25px"></div>
-<h2  style="text-align: center " ><u>Karte</u></h2>
-<!--The div element for the map -->
-<div id="map" style="width: 900px; height: 450px;border-radius: 20px; margin: auto"></div>
-<script src="js/main.js"></script>
+    <h2 style="text-align: center;" ><u>Haltestellen</u></h2>
+    <div id="detailRoute" style="min-height: 25px;height: auto; margin-left: auto; margin-right: auto; width: 25em; border-color: darkgrey; border-width: 2px; border-style: solid; background-color: ivory"></div>
+<hr>
+    <h2  style="text-align: center " ><u>Karte</u></h2>
+    <!--The div element for the map -->
+    <div id="map" style="width: 900px; height: 450px;border-radius: 20px; margin: auto"></div>
+    <script src="js/main.js"></script>
 </div>
 </body>
 </html>
