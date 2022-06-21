e5454261f5872bc63c36abab1a6aac28561eec8d
kousman <ko.scheiner@gmail.com>
2022-06-21 14:15:53 +0200

    Cleaned up and refactored js and html, improved ui, added loading spinner

--- index.php
+++ index.php
@@ -5,57 +5,57 @@
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css" rel="stylesheet">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js"></script>
+    <link rel="stylesheet/less" type="text/css" href="css/main.scss" />
+    <script src="https://cdn.jsdelivr.net/npm/less@4" ></script>
     <style>
-        @font-face {
-            font-family: "roboto";
-            src: url("fonts/Roboto-Regular.ttf");
-        }
-        p, span, h1, h2 {
-            font-family: "roboto", sans-serif;
-        }
-        hr {
-            margin: 45px;
-        }
-        input, select, option {
-            font-size: 20px;
-        }
     </style>
 </head>
-<body style="background-color: white; padding: 0 20%;margin: 0; font-size: 20px">
-<div style="background-color: aliceblue">
+<body>
+<div style="background-color: aliceblue; width: 100%;display: flex; flex-direction: column; justify-content: center; align-items: center;">
 
-<div style=" background-image: url(https://www.wvv.de/media-wvv/mobilitaet/bilder/bus-und-strassenbahn/wvv_elektrobus.jpg);background-repeat: no-repeat; height: 400px; background-size: cover; background-position: center">
-    <img src="https://legalhelper.eu/ccm19/public/logo/5c5986e/bdf04a6/968e274" style="text-align:left; padding-left: 10px; height: 90px; width: 190px">
-
+<div id="backgroundImageContainer">
+    <img id="backgroundImage" src="https://legalhelper.eu/ccm19/public/logo/5c5986e/bdf04a6/968e274">
 </div>
 
-      <h1 style="text-align: center "><u>Fahrplan</u></h1>
+    <h1><u>Fahrplan</u></h1>
 
-    <p style="text-align: center">
-        <label for="Route">Wähle einen Routentyp:</label>
-        <select id="Route" name="Route">
-            <option value="direct">direkt</option>
-            <option value="indirect">indirekt</option>
-        </select>
-    </p>
+    <div id="container">
+        <div class="selectContainer">
+            <label for="Route">Wähle einen Routentyp</label> <br>
+            <select name="Route" id="Route">
+                <option value="direct">direkt</option>
+                <option value="indirect">indirekt</option>
+            </select>
+        </div>
 
-    <p style="text-align: center">
-        <label for="Route">Wähle eine Abfahrtszeit:</label>
-        <input type="time" id="departTime" name="departTime">
-    </p>
+        <div class="selectContainer">
+            <label for="Route">Wähle eine Abfahrtszeit</label><br>
+            <input type="time" id="departTime" name="departTime" value="09:00">
+        </div>
+    </div>
 
-    <p style="text-align: center">
-        <input type="text" placeholder="Start" id="startStop" name="startStop">
-        <input type="text" placeholder="Ziel" id="endStop" name="endStop">
-        <input type="button" onclick="onSearch()" value="Suche">
-    </p>
+    <div id="formContainer">
+        <div class="form__group field">
+            <input autocomplete="false" type="input" class="form__field" placeholder="Start" name="name" id="startStop" />
+            <label for="name" class="form__label">Start</label>
+        </div>
+        <div class="form__group field">
+            <input autocomplete="false" type="input" class="form__field" placeholder="Start" name="name" id="endStop" />
+            <label for="name" class="form__label">Ziel</label>
+        </div>
+        <input id="searchButton" type="button" onclick="onSearch()" value="Suchen">
+    </div>
 <hr>
-    <h2 style="text-align: center;" ><u>Haltestellen</u></h2>
-    <div id="detailRoute" style="min-height: 25px;height: auto; margin-left: auto; margin-right: auto; width: 25em; border-color: darkgrey; border-width: 2px; border-style: solid; background-color: ivory"></div>
+    <h2><u>Haltestellen</u></h2>
+    <div id="detailRoute">
+        <div id="spinnerContainer">
+            <svg id="loadingSpinner" class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+            </svg>
+        </div>
+    </div>
 <hr>
-    <h2  style="text-align: center; margin-bottom: 45px" ><u>Karte</u></h2>
-    <!--The div element for the map -->
-    <div id="map" style="width: 90%; height: 450px;border-radius: 20px; margin: auto"></div>
+    <div id="map" style="width: 100%; height: 450px;margin-bottom: 25px;"></div>
     <script src="js/main.js"></script>
 </div>
 </body>
